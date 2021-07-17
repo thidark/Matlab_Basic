10486296c96e15d7bb4a862f42e677d13e23081a
Thidar Khaing <34748213+thidark@users.noreply.github.com>
2021-07-17 23:52:37 +0630

    Create main.m

---
+++ Degree_Radian/main.m
@@ -0,0 +1,11 @@
+r='Enter Radian : ';
+radian=input(r);
+degree=Radian2Degree(radian);
+fprintf('Degree Value = %.2f \n',degree)
+
+d='Enter Degree : ';
+degree1=input(d);
+radian=Radian2Degree(degree1);
+
+fprintf('Radian Value= %.2f \n',radian)
+
