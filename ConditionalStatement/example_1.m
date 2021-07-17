a601039e60533ff79b9adc7141f32663b209e1bf
Thidar Khaing <34748213+thidark@users.noreply.github.com>
2021-07-17 23:50:02 +0630

    Create example_1.m

---
+++ ConditionalStatement/example_1.m
@@ -0,0 +1,7 @@
+clc,clear;
+a=30;
+% check the conditoin using if statement
+if a<20 % if condition is true then print the following
+fprintf('a is less than 20 \n');
+end
+fprintf('value of a is : %d\n',a);
