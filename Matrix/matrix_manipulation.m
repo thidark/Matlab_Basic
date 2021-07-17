37c2c484d5d76428a46c94fa1be8576c5bcdc8fc
Thidar Khaing <34748213+thidark@users.noreply.github.com>
2021-07-17 23:46:56 +0630

    Add files via upload

---
+++ Matrix/matrix_manipulation.m
@@ -0,0 +1,21 @@
+% matrix multiplication
+A=[20 23 45; 3 4 5];
+B=[4 5 6 7; 1 2 3 4;10 20 30 40];
+C=A*B
+
+x=[1 2 3 4];
+y=[2 3 4 5];
+%z=x*y; % error
+zz=x.*y ; % element-wise [2 6 12 20]
+yy=y./x;
+
+xx=x.^3;
+
+
+
+
+
+
+
+
+
