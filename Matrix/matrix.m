2b3277e4320f8dcf473b59d6f28946a7334e498b
Thidar Khaing <34748213+thidark@users.noreply.github.com>
2021-07-17 23:46:01 +0630

    Create matrix.m

---
+++ Matrix/matrix.m
@@ -0,0 +1,12 @@
+A=[20 30 40; 50 60 70; 11 22 33; 44 55 66];
+Mi=A(2,3); % row 2 column 3
+m2=A(2,:)% row 2 all column
+m3=A(:,3) % all row column 3
+mm=A(2:4,1:2) 
+A(2:4,1:2)=0
+
+A=magic(3)% sum are equal ( diagonal or straight)
+
+% Operator ( >, >=,<,<=,==,~=), (&&,||,~)
+B=magic(3)
+
